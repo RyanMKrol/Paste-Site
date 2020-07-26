b57e5b9af3b5f2910985bb1c24e00c54c2ee0695
Ryan Krol <ryankr@hotmail.co.uk>
2020-07-26 19:24:23 +0100

    Add shared jumbotron component

---
+++ src/Components/PasteJumbotron.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Jumbotron from 'react-bootstrap/Jumbotron'
+
+export function PasteJumbotron() {
+  return (
+    <div>
+      <Jumbotron>
+        <h1>Save Your Pastes!</h1>
+        <p>This is a basic site for saving text online to retrieve later.</p>
+      </Jumbotron>
+    </div>
+  )
+}
---
+++ src/Components/index.js
@@ -0,0 +1,1 @@
+export { PasteJumbotron } from './PasteJumbotron'
